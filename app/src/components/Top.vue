f3616d0f6dee4947d926e234f7a34ecb300dbb2a
suna88 <alotofthings88@gmail.com>
2018-09-14 19:00:33 +0900

    first commit

---
+++ app/src/components/Top.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <h3>テストあぷり</h3>
+    <GmapMap
+      :center="{lat:35.6808, lng:139.719}"
+      :zoom="18"
+      map-type-id="terrain"
+      class="map"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center=m.position"></GmapMarker>
+    </GmapMap>
+    <button @click="move">スタート</button>
+  </div>
+</template>
+<style scoped lang="scss">
+  .container {
+    text-align: center;
+
+    .map {
+      width: 700px;
+      height: 500px;
+      margin: 0 auto;
+    }
+
+  }
+
+</style>
+
+<script>
+  // eslint-disable-next-line indent
+  /* eslint-disable indent */
+
+  export default {
+    name: 'Top',
+    data() {
+      return {
+        msg: 'message',
+        markers: [{
+          position: {
+            lat: 35.6,
+            lng: 140
+          }
+        }, {
+          position: {
+            lat: 35.6808,
+            lng: 139.719
+          }
+        }]
+      }
+    },
+    methods: {
+      move: function () {
+        setInterval(() => {
+          this.markers[1].position.lat += 0.00001
+          this.markers[1].position.lng += 0.000003
+          console.log(this.markers[1].position.lng)
+        }, 500)
+      }
+    }
+  }
+</script>
---
+++ app/src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyA8VqKz4e08LMXck6GmHeoRFYv8U62s-NM',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {App},
+  template: '<App/>'
+})
